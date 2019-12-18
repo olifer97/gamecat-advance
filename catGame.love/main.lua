3078cfda891c4c64772888840fa364203190cfd5
Oli <olifer97@gmail.com>
2019-12-18 00:10:28 -0300

    Foto de bibloteca. Agrego limites en los bordes y bibloteca en movimiento.

--- catGame.love/main.lua
+++ catGame.love/main.lua
@@ -1,6 +1,8 @@
 local cat = {}
 local platform = {}
 
+local library = {}
+
 local gravity = -500
 
 function updateCatframes()
@@ -24,11 +26,11 @@ function love.load()
 	platform.height = love.graphics.getHeight()  -- This makes the platform as tall as the whole game window.
  
         -- This is the coordinates where the platform will be rendered.
-	platform.x = 0                               -- This starts drawing the platform at the left edge of the game window.
+	platform.x = 0                              -- This starts drawing the platform at the left edge of the game window.
 	platform.y = 350            -- This starts drawing the platform at the very middle of the game window
 
     --starting point cat
-    cat.x = 50;
+    cat.x = 60;
     cat.y = 290;
     cat.img = {}
     cat.img[1] = love.graphics.newImage("cat-frame-0.png")
@@ -36,19 +38,27 @@ function love.load()
     cat.img[3] = love.graphics.newImage("cat-frame-2.png")
     cat.ground = cat.y
     cat.y_velocity = 0
-    cat.jump_height = -200
+    cat.jump_height = -250
     cat.currentFrame = 1
     cat.activeFrame = cat.img[cat.currentFrame]
     cat.mirror = 1
     cat.speed = 100
+    cat.width = cat.img[1]:getWidth() * 3
+
+    --library
+    library.x = love.graphics.getWidth()
+    library.y = 133
+    library.speed = 80
+    library.img = love.graphics.newImage("library.png")
 end
 
 function love.draw()
+    love.graphics.draw(library.img, library.x, library.y, 0 , 0.8,0.8)
     love.graphics.setColor(1, 1, 1)        -- This sets the platform color to white.
- 
-        -- The platform will now be drawn as a white rectangle while taking in the variables we declared above.
+     -- The platform will now be drawn as a white rectangle while taking in the variables we declared above.
     love.graphics.rectangle('fill', platform.x, platform.y, platform.width, platform.height)
     
+
     love.graphics.draw(cat.activeFrame,cat.x,cat.y,0,cat.mirror * 3,3)
 end
 
@@ -56,8 +66,13 @@ local elapsedTime = 0
 function love.update(dt)
     elapsedTime = elapsedTime + dt
 
+    library.x = library.x - (library.speed * dt)
+
     if love.keyboard.isDown('right') then 
         cat.x = cat.x + (cat.speed * dt)
+        if(cat.x + cat.width > love.graphics.getWidth()) then
+            cat.x = cat.x - (cat.speed * dt)
+        end
         --if(elapsedTime > 0.1) then
         cat.mirror = 1
         updateCatframes()
@@ -66,6 +81,9 @@ function love.update(dt)
         
 	elseif love.keyboard.isDown('left') then
         cat.x = cat.x - (cat.speed * dt)
+        if(cat.x - cat.width < 0) then
+            cat.x = cat.x + (cat.speed * dt)
+        end
         --if(elapsedTime > 0.1) then
         cat.mirror = -1
         updateCatframes()
